11b4547d5a4dfef10e69f20c367273b728ef8aa5
aymanalshiekh <aymanalshiekh1@gmail.com>
2023-02-04 23:41:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# twitteri-n-tailwindcss
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'], //path for all files i want tailwind to scan it
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
